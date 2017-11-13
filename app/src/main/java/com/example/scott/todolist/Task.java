46e16b2bda9914d8cde92cb7cbd7c3aa84fabf47
Scott Murray <rai.five@gmail.com>
2017-11-13 16:34:17 +0000

    Task Class constructor & getter methods added

--- app/src/main/java/com/example/scott/todolist/Task.java
+++ app/src/main/java/com/example/scott/todolist/Task.java
@@ -10,5 +10,33 @@ public class Task {
     private String category;
     private String description;
     private boolean completeStatus;
+
+    public Task(Integer id, String name, String category, String description, boolean completeStatus) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.completeStatus = completeStatus;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleteStatus() {
+        return completeStatus;
+    }
 }
 
