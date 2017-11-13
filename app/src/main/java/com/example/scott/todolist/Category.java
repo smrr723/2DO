1f54968fe1145e502a90d6b8965de91a4acf192b
Scott Murray <rai.five@gmail.com>
2017-11-13 16:21:46 +0000

    Initial Commit - Adding Task & Category Classes

---
+++ app/src/main/java/com/example/scott/todolist/Category.java
@@ -0,0 +1,8 @@
+package com.example.scott.todolist;
+
+/**
+ * Created by scott on 12/11/2017.
+ */
+
+public class Category {
+}
---
+++ app/src/main/java/com/example/scott/todolist/Task.java
@@ -0,0 +1,14 @@
+package com.example.scott.todolist;
+
+/**
+ * Created by scott on 12/11/2017.
+ */
+
+public class Task {
+    private Integer id;
+    private String name;
+    private String category;
+    private String description;
+    private boolean completeStatus;
+}
+
