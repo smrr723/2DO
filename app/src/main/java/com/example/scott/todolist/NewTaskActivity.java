2772c10d40d3d6d9876bb8bd0df68c513f100e7e
Scott Murray <rai.five@gmail.com>
2017-11-13 20:46:39 +0000

    Added layout items and constraints to activity_new_task

--- app/src/main/java/com/example/scott/todolist/NewTaskActivity.java
+++ app/src/main/java/com/example/scott/todolist/NewTaskActivity.java
@@ -3,11 +3,11 @@ package com.example.scott.todolist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TaskActivity extends AppCompatActivity {
+public class NewTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task);
+        setContentView(R.layout.activity_new_task);
     }
 }
