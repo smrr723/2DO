de4d51f2bdc456cb6f3df56789608b916982f8d2
Scott Murray <rai.five@gmail.com>
2017-11-14 13:24:09 +0000

    Added MyMenu file

---
+++ app/src/main/java/com/example/scott/todolist/MyMenu.java
@@ -0,0 +1,30 @@
+package com.example.scott.todolist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+/**
+ * Created by scott on 14/11/2017.
+ */
+
+public class MyMenu extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_to_do_list, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.addTask){
+            Intent intent = new Intent(this, NewTaskActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
