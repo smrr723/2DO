dd6dfd2de8d18a200abb065112a5fd21d279e644
Scott Murray <rai.five@gmail.com>
2017-11-15 10:46:27 +0000

    Splash screen working & delay added using handler.postDelayed()

--- app/src/main/java/com/example/scott/todolist/HomeActivity.java
+++ app/src/main/java/com/example/scott/todolist/HomeActivity.java
@@ -1,13 +1,25 @@
 package com.example.scott.todolist;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(HomeActivity.this, ToDoListActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
