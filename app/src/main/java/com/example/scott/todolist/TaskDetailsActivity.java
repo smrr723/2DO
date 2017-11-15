186006c3c123b6dfbda970839813bc89595d60bb
Scott Murray <rai.five@gmail.com>
2017-11-15 10:38:46 +0000

    TaskDetailsActivity transferred from v2

---
+++ app/src/main/java/com/example/scott/todolist/TaskDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.example.scott.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_details);
+    }
+}
